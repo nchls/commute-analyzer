a4935490a5fc7c022bfed95fd654ba25d03979d5
Ian <ian@nchls.com>
2016-08-05 21:53:32 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+window.init = function() {
+	var directionsService = new google.maps.DirectionsService;
+
+	directionsService.route({
+		origin: '25 Pearl St., Portland, ME',
+		destination: '354 Brown Road, Pownal, ME',
+		travelMode: 'DRIVING'
+	}, function(response, status) {
+		console.log('response!', response);
+	});
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+	var gmScript = document.createElement('script');
+	gmScript.src = 'https://maps.googleapis.com/maps/api/js?key=' + window.apiKey + '&callback=init';
+	document.body.appendChild(gmScript);
+});
