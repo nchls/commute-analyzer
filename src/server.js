477d9e305b67fb9c224f31511d1c4c681eb46ce0
Ian <ian@nchls.com>
2016-08-27 23:57:53 +0000

    Hapi web server, socket.io

--- src/server.js
+++ src/server.js
@@ -1,41 +1,31 @@
-var WebSocketServer = require('websocket').server;
-var http = require('http');
+const hapi = require('hapi');
+const socketIo = require('socket.io');
 
-var server = http.createServer(function(req, res) {
-	console.log((new Date()) + ' Received request for ' + req.url);
-	response.writeHead(404);
-	response.end();
+const server = new hapi.Server();
+
+server.connection({
+	port: 29090
 });
 
-server.listen(8080, function() {
-	console.log((new Date()) + ' Server is listening on port 8080');	
-});
+const io = socketIo(server.listener, {serveClient: false});
 
-var wsServer = new WebSocketServer({
-	httpServer: server,
-	autoAcceptConnections: false
-});
-
-function originIsAllowed(origin) {
-	return true;
-}
-
-wsServer.on('request', function(req) {
-	if (!originIsAllowed(request.origin)) {
-		req.reject();
-		console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-		return;
-	}
-
-	var connection = request.accept('echo-protocol', request.origin);
-	console.log((new Date()) + ' Connection accepted.');
-	connection.on('message', function(message) {
-		if (message.type === 'utf8') {
-			console.log('Received message: ' + message.utf8Data);
-			connection.sendUTF(message.utf8Data);
-		} else if (message.type === 'binary') {
-			console.log('Received binary message of ' + message.binaryData.length + ' bytes');
-			
-		}
+io.on('connection', function(socket) {
+	socket.emit('oh hi!');
+	socket.on('burp', function() {
+		socket.emit('excuse you');
 	});
+});
+
+/*
+server.route({
+	method: 'GET',
+	path: '/',
+	handler: function(request, reply) {
+		reply('route to api');
+	}
+});
+*/
+
+server.start(function() {
+	console.log('Server running at:', server.info.uri);
 });
