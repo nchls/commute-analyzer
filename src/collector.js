98ddffb8963ae9bfee55db0ec8cd8b72c05cf050
Ian <ian@nchls.com>
2016-09-06 01:09:25 -0400

    Mapping steps

--- src/collector.js
+++ src/collector.js
@@ -10,6 +10,7 @@ var Trip = require('./models/Trip');
 var BaseStep = require('./models/BaseStep');
 var Step = require('./models/Step');
 
+var SECONDS_TO_GET_IN_YOUR_CAR_AND_START_DRIVING = 90;
 var VERBOSE = true;
 
 var TIMEZONE = 'America/New_York';
@@ -32,8 +33,10 @@ function init() {
 		getRoutesAndBaseSteps(destinationType).then(function(routesAndBaseSteps) {
 			_.forEach(routesAndBaseSteps, function(baseSteps, routeId) {
 				createTrip(routeId, destinationType).then(function(tripResult) {
-					mapStep(tripResult[0].rows[0].id, baseSteps, new Date().getTime()).then(function(stepResult) {
-						console.log('Steps mapped correctly', stepResult);
+					var tripId = tripResult[0].rows[0].id;
+					var timestamp = new Date().getTime() + SECONDS_TO_GET_IN_YOUR_CAR_AND_START_DRIVING;
+					mapStep(tripId, baseSteps, timestamp).then(function(stepResult) {
+						// Steps mapped correctly
 					}).catch(function(error) {
 						logError('Error in step mapping', error);
 					});
@@ -53,7 +56,7 @@ function init() {
 
 function getRoutesAndBaseSteps(destinationType) {
 	var sql = [
-		'select "Route".id as "routeId", "BaseStep".index, "BaseStep"."startLat", "BaseStep"."startLon", "BaseStep"."endLat", "BaseStep"."endLon"',
+		'select "Route".id as "routeId", "BaseStep".id as "baseStepId", "BaseStep".index, "BaseStep"."startLat", "BaseStep"."startLon", "BaseStep"."endLat", "BaseStep"."endLon"',
 		'from "Route", "BaseTrip", "BaseStep"',
 		'where "BaseTrip"."destinationType" = $1',
 		'and "BaseTrip".route = "Route".id',
@@ -64,7 +67,6 @@ function getRoutesAndBaseSteps(destinationType) {
 	return new Promise(function(resolve, reject) {		
 		db.rawQuery(sql, [destinationType]).then(function(result) {
 			var output = _.groupBy(result.rows, 'routeId');
-			console.log('output', output);
 			resolve(output);
 		}).catch(function(error) {
 			reject(error);
@@ -84,81 +86,46 @@ function createTrip(routeId, destinationType) {
 function mapStep(tripId, steps, timestamp) {
 	return new Promise(function(resolve, reject) {
 		var step = steps.shift();
-
+		if (step === undefined) {
+			resolve();
+		}
+		var origin = step.startLat + ',' + step.startLon;
+		var destination = step.endLat + ',' + step.endLon;
+		axios.get('https://maps.googleapis.com/maps/api/directions/json', {
+			params: {
+				key: pvt.serverKey,
+				origin: origin,
+				destination: destination,
+				departure_time: timestamp
+			}
+		}).then(function(response) {
+			var mappedRoute = response.data.routes[0];
+			var warnings = mappedRoute.warnings;
+			if (warnings.length) {
+				log('warnings!', warnings);
+			}
+			var leg = mappedRoute.legs[0];
+			var duration = leg.duration_in_traffic.value;
+			var stepPayload = [{
+				baseStep: step.baseStepId,
+				trip: tripId,
+				duration: duration
+			}];
+			db.insert(Step, 'Step', stepPayload).then(function() {
+				mapStep(tripId, steps, timestamp + duration).then(resolve).catch(function(error) {
+					reject(error);
+				});
+			}).catch(function(error) {
+				reject(error);
+			});
+		}).catch(function(error) {
+			logError('Google Maps API call error!', error);
+			reject(error);
+		});
 	});
 }
 
-function mapRoute(route) {
-	var origin;
-	var destination;
-	var destinationType;
-
-	if (isMorning) {
-		origin = route.home;
-		destination = route.office;
-		destinationType = 'office';
-	} else if (isEvening) {
-		origin = route.office;
-		destination = route.home;
-		destinationType = 'home';
-	}
-
-	if (origin !== undefined) {
-		axios.get('https://maps.googleapis.com/maps/api/directions/json', {
-				params: {
-					key: pvt.serverKey,
-					origin: origin,
-					destination: destination
-				}
-			})
-			.then(function(response) {
-				var mappedRoute = response.data.routes[0];
-				var warnings = mappedRoute.warnings;
-				if (warnings.length) {
-					log('warnings!', warnings);
-				}
-				var trip = mappedRoute.legs[0];
-				var steps = trip.steps;
-
-				var tripPayload = {
-					route: route.id,
-					destinationType: destinationType
-				};
-				var tripPromise = db.insert(Trip, 'Trip', [tripPayload]);
-
-				tripPromise.then(function(tripResult) {
-					var tripId = tripResult[0].rows[0].id;
-					log('Inserted new trip ID', tripId);
-					var stepPayload = steps.map(function(step, index) {
-						return {
-							trip: tripId,
-							index: index,
-							duration: step.duration.value,
-							coords: JSON.stringify(step.start_location) + JSON.stringify(step.end_location)
-						};
-					});
-					db.insert(Step, 'Step', stepPayload)
-						.then(function(stepResult) {
-							if (VERBOSE) {
-								log('Inserted new step IDs', stepResult.map(function(result) {
-									return result.rows[0].id;
-								}));
-							}
-						}).catch(function(error) {
-							logError('Step insert failed!', error);
-						});
-				}).catch(function(error) {
-					logError('Trip insert failed!', error);
-				});
-			})
-			.catch(function(error) {
-				logError('Google Maps API call error!', error);
-			});
-	}
-}
-
 function isInMorningCommuteWindow(momentInstance) {
-	return true;
 	var morningStart = moment().tz(TIMEZONE).set({
 		hour: 5,
 		minute: 0,
